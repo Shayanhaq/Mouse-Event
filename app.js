b75759885fb66c2165c19a780622a5de07dc0082
Shayanhaq <haqshayan2@gmail.com>
2024-06-27 02:26:19 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const image = document.getElementById("myImage");
+
+image.addEventListener("mouseover", () => {
+  image.src = "Lamborghini Huracan Lp Black Dark 4k Wallpaper HD.jpg";
+});
+
+image.addEventListener("mouseout", () => {
+  image.src = "Dark Moon 4K Wallpaper Gallery.jpg";
+});
